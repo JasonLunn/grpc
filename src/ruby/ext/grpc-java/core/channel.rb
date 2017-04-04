8dc370cb37b8f5530ccbc84c078848fa4af882b7
Jason Lunn <jlunn@jungroup.com>
2017-04-03 20:34:37 -0400

    Get channel_spec to pass

--- src/ruby/ext/grpc-java/core/channel.rb
+++ src/ruby/ext/grpc-java/core/channel.rb
@@ -1,11 +1,31 @@
 module GRPC
   module Core
     class Channel
-      def initialize host, *args
+      SSL_TARGET = :SSL_TARGET
       
+      def initialize host, options, credentials
+        if credentials.is_a? Symbol and credentials != :this_channel_is_insecure
+          raise TypeError.new 'bad creds symbol, want :this_channel_is_insecure'
+        end
+        if options.is_a? Hash
+          raise TypeError.new 'Invalid key' unless options.keys.map( &:class ).all? { | clazz | [ String, Symbol ].include? clazz }
+          raise TypeError.new 'Invalid value' unless options.values.map( &:class ).all? { | clazz | [ String, Symbol, Fixnum ].include? clazz }
+        end
+        @connectivity_state = ConnectivityStates::IDLE
+        @closed = false
       end
       def create_call *args
+        raise 'Already closed' if @closed
         Call.new
+      end
+      def destroy
+      
+      end
+      def close *args
+        @closed = true
+      end
+      def connectivity_state *args
+        @connectivity_state
       end
     end
   end
--- src/ruby/ext/grpc-java/core/channel_credentials.rb
+++ src/ruby/ext/grpc-java/core/channel_credentials.rb
@@ -1,6 +1,9 @@
 module GRPC
   module Core
     class ChannelCredentials
+      def initialize *args
+      
+      end
       @@default_roots_pem = nil
       def self.set_default_roots_pem default_roots_pem
         @@default_roots_pem = default_roots_pem
